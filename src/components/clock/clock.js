9b955948a0444742b363762550a8b493abc9a4f1
Zhoraev D <newlifenewgoal@gmail.com>
2021-05-20 23:49:52 +0600

    добавлена рабочая дигатал часы

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './clock.css';
 
 const Clock = () => {
-    return <span className="clock">8:33:02</span>
+    const [time, setTime] = useState(new Date());
+
+    let interval;
+
+    useEffect(() => {
+        interval = setInterval(() => {
+            setTime(new Date())
+        }, 1000);
+
+        return () => clearInterval(interval);
+    });
+
+    return <div className="clock">{time.toLocaleTimeString()}</div>
 }
 
 export default Clock;
